aa57d2f9c682eace0e313ee64df9e8ef3e7158e9
Sooraj <soorajnandan95@gmail.com>
2024-11-27 18:05:14 +0530

    Login API TEST
    Created Login API Test Class

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -2,8 +2,7 @@ package com.api.tests;
 
 public class LoginAPITest {
 
-	public void loginAPITest()
-	{
-	System.out.println("Running Login API Test");
+	public void loginAPITest() {
+		System.out.println("Running Login API Test");
 	}
 }
